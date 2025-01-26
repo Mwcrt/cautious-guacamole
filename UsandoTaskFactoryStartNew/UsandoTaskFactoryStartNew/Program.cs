577d33f0b501980faaf9c2a9484c5aa98bb67909
Mwcrt <165202406+Mwcrt@users.noreply.github.com>
2025-01-26 11:00:37 -0400

    Add files via upload

---
+++ UsandoTaskFactoryStartNew/UsandoTaskFactoryStartNew/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static async Task Main()
+    {
+        Task task = Task.Factory.StartNew(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Tarea ejecutándose: {i}");
+                Task.Delay(500).Wait(); 
+            }
+        }, TaskCreationOptions.LongRunning);
+
+        // Necesitamos esperar a que la tarea termine para mostrar el mensaje de que la tarea fue completada
+        await task;
+
+        Console.WriteLine("Tarea completada.");
+    }
+}

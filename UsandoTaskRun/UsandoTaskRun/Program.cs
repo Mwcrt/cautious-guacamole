f7f88fc94baa3f4c5f67c1247d0524ac0f5e46c4
Mwcrt <165202406+Mwcrt@users.noreply.github.com>
2025-01-26 11:00:08 -0400

    Add files via upload

---
+++ UsandoTaskRun/UsandoTaskRun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task_Run
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Tarea iniciada.");
+
+            Task task = Task.Run(() =>
+            {
+                long sum = 0;
+                Console.WriteLine("Iniciando");
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += i * 2;
+                }
+                Console.WriteLine($"Se ha finalizado con la suma total a: {sum}");
+            });
+
+            await task; // Espera a que la tarea termine
+
+            Console.WriteLine("Tarea completada.");
+        }
+    }
+}
